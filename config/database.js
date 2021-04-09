14e0ffa053a25c46f1e6cfaa335b01a564ffff62
Jessalynn Moncada <moncadaj@Jessalynns-MacBook-Air.local>
2021-04-09 16:53:32 -0700

    first commit

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, {
+//    console.log('process.env.DATABASE_URL')
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true
+});
+
+const db = mongoose.connection; 
+
+db.once('connected', function () {
+    console.log(`Connected To MongoDB at ${db.host}; ${db.port}`);
+}) 
+
