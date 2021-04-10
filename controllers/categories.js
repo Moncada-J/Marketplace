34886d074284c1bf98636e4fba9beb19d85097f2
Mark Oleo <oleo.mark@gmail.com>
2021-04-10 16:14:11 -0700

    Add routes, views,controllers

--- controllers/categories.js
+++ controllers/categories.js
@@ -2,11 +2,32 @@ const Category = require('../models/category');
 
 module.exports = {
     index,
+    show, 
+    new: newListing,
+    create, 
 };
 
 function index(req, res) {
     Category.find({}, function(err, categories) {
         console.log(categories);
         res.render('categories/index', {title: "My Categories", categories});
+    });
+}
+
+function show(req, res) {
+    Category.findById(req.params.id, function (err, category) {
+        res.render("categories/show", {category});
+    });
+}
+
+function newListing(req, res) {
+    res.render('categories/new', {title:"Create Listing"});
+}
+
+function create(req, res) {
+    const listing = new Listing(req.body);
+    listing.save(function(err) {
+        if(err) return res.redirect('/categories/new');
+        res.redirect(`/categories/${categorie._id}`);
     });
 }
--- models/category.js
+++ models/category.js
@@ -2,6 +2,23 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const itemSchema = new Schema({
+    fruits: {
+        type: String,
+        enum: ["Apples","Bananas","Mangos", "Oranges", "Pineapple"],
+    },
+    vegetables: {
+        type: String,
+        enum: ["Artichoke", "Broccoli", "Carrots", "Potatoes", "Tomatoes"],
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 5, 
+        max: 7,
+    }
+});
+
 const categorySchema = new Schema ({
     variety: {
         type: String,
@@ -12,6 +29,7 @@ const categorySchema = new Schema ({
         min: 0,
         max: 10,
     },
+    items: [itemSchema],
 });
 
 module.exports = mongoose.model("Category", categorySchema);
