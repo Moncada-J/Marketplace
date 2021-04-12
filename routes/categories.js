be19b24183b28f0ce6e6101b17a847bca62c91d5
Jessalynn Moncada <moncadaj@Jessalynns-MacBook-Air.local>
2021-04-12 10:07:03 -0700

    adds back routes to categories.js

--- routes/categories.js
+++ routes/categories.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 const categoriesCtrl = require('../controllers/categories');
 
-router.get('/', categoriesCtrl.index);
+router.get("/", categoriesCtrl.index);
+router.get("/new", categoriesCtrl.new);
+router.get("/:id", categoriesCtrl.show);
+router.post("/", categoriesCtrl.create);
 
 
 module.exports = router;
