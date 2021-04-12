141dee9a4074cd29663be636ef2f68988c54b0da
Mark Oleo <oleo.mark@gmail.com>
2021-04-12 15:50:16 -0700

    4/12 Changes

--- controllers/categories.js
+++ controllers/categories.js
@@ -28,7 +28,9 @@ function show(req, res, next) {
 
 function create(req, res) {
     const listing = new Listing(req.body);
+    console.log("Listing test works");
     listing.save(function(err) {
+        console.log("Saving test works");
         if(err) return res.redirect('/categories/new');
         res.redirect(`/categories/${categories._id}`);
     });
