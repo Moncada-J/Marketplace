53470134b8b79cd7303a62ec2e169208f2c51f1d
Mark Oleo <oleo.mark@gmail.com>
2021-04-11 22:08:58 -0700

    Routes edit

--- routes/categories.js
+++ routes/categories.js
@@ -3,6 +3,9 @@ var router = express.Router();
 const categoriesCtrl = require('../controllers/categories');
 
 router.get('/', categoriesCtrl.index);
+router.get('/new', categoriesCtrl.new);
+router.get('/:id', categoriesCtrl.show);
+router.post('/', categoriesCtrl.create);
 
 
 module.exports = router;
