68a70907eda128f9eff10f4e90883c1f868de1e3
Jessalynn Moncada <moncadaj@Jessalynns-MacBook-Air.local>
2021-04-12 15:47:29 -0700

    fixes schema structure and changes forms to hrefs

--- models/category.js
+++ models/category.js
@@ -3,33 +3,34 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const itemSchema = new Schema({
-    fruits: {
-        type: String,
-        enum: ["Apples","Bananas","Mangos", "Oranges", "Pineapple"],
-    },
-    vegetables: {
-        type: String,
-        enum: ["Artichoke", "Broccoli", "Carrots", "Potatoes", "Tomatoes"],
-    },
-    price: {
-        type: Number,
-        required: true,
-        min: 5, 
-        max: 7,
-    }
+  fruits: {
+    type: String,
+    enum: ["Apples", "Bananas", "Mangos", "Oranges", "Pineapple"],
+  },
+  vegetables: {
+    type: String,
+    enum: ["Artichoke", "Broccoli", "Carrots", "Potatoes", "Tomatoes"],
+  },
+  quantity: {
+    type: Number,
+    min: 1,
+    max: 10,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 5,
+    max: 7,
+  },
 });
 
-const categorySchema = new Schema ({
-    variety: {
-        type: String,
-        enum: ["Fruits", "Vegetables"]
-    },
-    quantity: {
-        type: Number,
-        min: 1,
-        max: 10,
-    },
-    items: [itemSchema],
+const categorySchema = new Schema({
+  variety: {
+    type: String,
+    enum: ["fruits", "vegetables"]
+  },
+  items: [itemSchema]
 });
 
 module.exports = mongoose.model("Category", categorySchema);
+
